4052fbe845476c076eddf0308b98eee176b14c0d
Marcos Alves <marcosalvesjr007@gmail.com>
2023-03-24 01:47:29 +0000

    Finalização e update

---
+++ Vetores/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        String[] nomes = new String[7];
+
+        nomes[0] = "Marcos";
+        nomes[1] = "Junior";
+        nomes[2] = "Marcos";
+        nomes[3] = "Junior";
+        nomes[4] = "Marcos";
+        nomes[5] = "Junior";
+        nomes[6] = "Marcos";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
